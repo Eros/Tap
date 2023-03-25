932744e60452b38cb67d155401f46a748840febf
Eros <george.marr.cobc@gmail.com>
2023-03-25 17:40:45 +0000

    move things to use a class instead

--- src/tap.ts
+++ src/tap.ts
@@ -1,120 +1,134 @@
-import * as net from "net";
 import {ServerInfo} from "./impl/ServerInfo";
+import * as net from "net";
 import * as dns from "dns";
 import * as dnsPromises from 'dns/promises'
 
-/**
- * Gets the servers information from a dns name rather
- * than using a specified IP address and port, uses the
- * {@link resolveDomain} method to try and find the servers
- * IP & port.
- *
- * @param dnsName to get the information from.
- */
-export async function getServerInformationFromDns(dnsName: string): Promise<ServerInfo> {
-    const dnsInfo = resolveDomain(dnsName);
+export class Tap {
 
-    return getServerInformation(dnsInfo[0], dnsInfo[1]);
-}
+    private readonly domain: string;
+    private readonly timeout: number;
 
-/**
- * Takes in the dns name of a server
- * and resolves it to obtain the
- * servers IP address and the port.
- * Defaults the port to 25565
- * if it cannot find it.
- *
- * @param domain to search and get the address from.
- * @param timeout the amount of time that it will
- *                attempt to get the information from
- *                before failing.
- */
-async function resolveDomain(domain: string, timeout: number = 5000): Promise<{address: string; port: number}> {
-    const srvPromise = dnsPromises.resolveSrv(`_minecraft._tcp.${domain}`);
-    const ipPromise = dnsPromises.resolve4(domain);
-
-    // Create a timeout that will reject after the specified time.
-    const timeoutPromise = new Promise<never>((_, reject) => {
-        setTimeout(() => reject(new Error('Request timed out')), timeout);
-    });
-
-    try {
-        // Attempt to resolve the SRV record, if successful return
-        // the servers address and port from the SRV record.
-        const srvResult: dns.SrvRecord[] = await Promise.race([srvPromise, timeoutPromise]);
-        return {
-            address: srvResult[0].name,
-            port: srvResult[0].port,
-        };
-    } catch (error) {
-        // If the error is due to a time out there's
-        // not much else to do so throw the error.
-        if (error.message === 'Request timed out') {
-            throw error;
-        }
-
-        // If the SRV record resolution failed for any reason then attempt
-        // to resolve the A record (IPv4) and default the port to 25565.
-        const ipAddress: string[] = await Promise.race([ipPromise, timeoutPromise]);
-        return {
-            address: ipAddress[0],
-            port: 25565,
-        };
+    /**
+     * @param domain of the server to get the information from.
+     * @param timeout before the attempts will fail.
+     */
+    constructor(domain: string, timeout: number) {
+        this.domain = domain;
+        this.timeout = timeout;
     }
-}
 
-/**
- * Sends a ping to the server and get the
- * information from the required data,
- * sends the ping by telling the server
- * that it's sending the 'Server List Ping' packet.
- * @param serverAddress to send the data to.
- * @param port to attach to.
- * @return {@link ServerInfo} with all provided data.
- */
-export async function getServerInformation(serverAddress: string, port: number = 25565): Promise<ServerInfo> {
-    return new Promise((resolve, reject) => {
-        const socket = new net.Socket();
+    /**
+     * Gets the server information from the
+     * domain name provided in the constructor.
+     * @return {@link ServerInfo}
+     */
+    public async fetchServerInfo(): Promise<ServerInfo> {
+        const {address, port} = await this.resolveDomain();
+        return await this.getServerInformation(address, port);
+    }
 
-        socket.setTimeout(5_000);
+    /**
+     * Gets the server information form a manually specified
+     * port and address rather than specifying a domain
+     * and a port.
+     * @param address
+     * @param port
+     * @return {@link ServerInfo}
+     */
+    public async _fetchServerInfo(address: string, port: number): Promise<ServerInfo> {
+        return await this.getServerInformation(address, port);
+    }
 
-        socket.on('data', (data) => {
-            const serverInfo = parseServerInfo(data);
+    /**
+     * Runs through the server information provided and
+     * creates a socket that will send a packet to the
+     * server pretending to be the server list ping request.
+     * @param address to get the information from.
+     * @param port to attach to.
+     */
+    private async getServerInformation(address: string, port: number): Promise<ServerInfo> {
+        return new Promise((resolve, reject) => {
+            const socket = new net.Socket();
 
-            const info: ServerInfo = {
-                name: serverAddress,
-                playerCount: parseInt(serverInfo['player_0'], 10),
-                motd: serverInfo['description'],
-                version: serverInfo['version']
+            socket.setTimeout(5_000);
+
+            socket.on('data', (data) => {
+                const serverInfo = this.parseServerInfo(data);
+
+                const info: ServerInfo = {
+                    name: address,
+                    playerCount: parseInt(serverInfo['player_0'], 10),
+                    motd: serverInfo['description'],
+                    version: serverInfo['version']
+                }
+
+                resolve(info);
+                socket.end();
+            });
+
+            socket.on('timeout', () => {
+                reject(new Error('Request timed out'));
+                socket.end();
+            });
+
+            socket.on('error', (err) => reject(err));
+
+            socket.connect(port, address, () => {
+                socket.write(Buffer.concat([
+                    Buffer.from([0xFE, 0x01]),
+                    Buffer.alloc(12, 0x00)
+                ]));
+            });
+        });
+    }
+
+    /**
+     * Takes a domain name and attempts to resolve the
+     * record into the IP address and the port address.
+     * @return the address and the port.
+     */
+    private async resolveDomain(): Promise<{ address: string; port: number }> {
+        const srvPromise = dnsPromises.resolveSrv(`_minecraft._tcp.${this.domain}`);
+        const ipPromise = dnsPromises.resolve4(this.domain);
+
+        // Create a timeout that will reject after the specified time.
+        const timeoutPromise = new Promise<never>((_, reject) => {
+            setTimeout(() => reject(new Error('Request timed out')), this.timeout);
+        });
+
+        try {
+            // Attempt to resolve the SRV record, if successful return
+            // the servers address and port from the SRV record.
+            const srvResult: dns.SrvRecord[] = await Promise.race([srvPromise, timeoutPromise]);
+            return {
+                address: srvResult[0].name,
+                port: srvResult[0].port,
+            };
+        } catch (error) {
+            // If the error is due to a time out there's
+            // not much else to do so throw the error.
+            if (error.message === 'Request timed out') {
+                throw error;
             }
 
-            resolve(info);
-            socket.end();
-        });
-
-        socket.on('timeout', () => {
-            reject(new Error('Request timed out'));
-            socket.end();
-        });
-
-        socket.on('error', (err) => reject(err));
-
-        socket.connect(port, serverAddress, () => {
-            socket.write(Buffer.concat([
-                Buffer.from([0xFE, 0x01]),
-                Buffer.alloc(12, 0x00)
-            ]));
-        });
-    });
-}
-
-function parseServerInfo(data: Buffer): Record<string, string> {
-    const payload = data.slice(5).toString('utf16le').split('\x00');
-    const info: Record<string, string> = {};
-
-    for (let i = 0; i < payload.length - 1; i += 2) {
-        info[payload[i]] = payload[i + 1];
+            // If the SRV record resolution failed for any reason then attempt
+            // to resolve the A record (IPv4) and default the port to 25565.
+            const ipAddress: string[] = await Promise.race([ipPromise, timeoutPromise]);
+            return {
+                address: ipAddress[0],
+                port: 25565,
+            };
+        }
     }
 
-    return info;
+    private parseServerInfo(data: Buffer): Record<string, string> {
+        const payload = data.slice(5).toString('utf16le').split('\x00');
+        const info: Record<string, string> = {};
+
+        for (let i = 0; i < payload.length - 1; i += 2) {
+            info[payload[i]] = payload[i + 1];
+        }
+
+        return info;
+    }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,23 +1,15 @@
-import {getServerInformation, getServerInformationFromDns} from './tap'
+import {Tap} from "./tap";
 
-getServerInformation('player.example.com')
-    .then((serverInfo) => {
-        console.log(`Name: ${serverInfo.name}`);
-        console.log(`Player Count: ${serverInfo.playerCount}`);
-        console.log(`MOTD: ${serverInfo.motd}`);
-        console.log(`Versions: ${serverInfo.version}`);
-    })
-    .catch((error) => {
-        console.error(`Error ${error}`);
-    });
+async function testInfo(domain: string) {
+    try {
+        const server = new Tap(domain, 5_000);
+        const serverInfo = await server.fetchServerInfo();
 
-getServerInformationFromDns('mc.hypixel.net')
-    .then((serverInfo) => {
         console.log(`Name: ${serverInfo.name}`);
-        console.log(`Player Count: ${serverInfo.playerCount}`);
+        console.log(`Player count: ${serverInfo.playerCount}`);
         console.log(`MOTD: ${serverInfo.motd}`);
-        console.log(`Versions: ${serverInfo.version}`);
-    })
-    .catch((error) => {
-        console.error(`Error ${error}`);
-    });
+        console.log(`Supported versions: ${serverInfo.version}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+}
